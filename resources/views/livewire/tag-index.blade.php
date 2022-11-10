6d17e30036cf0e9187defe7d5c46165063d6d5da
Aya Mohammed <aya59268@gmail.com>
2022-11-11 00:40:34 +0200

    Create Tag

--- resources/views/livewire/tag-index.blade.php
+++ resources/views/livewire/tag-index.blade.php
@@ -1,6 +1,6 @@
 <section class="container mx-auto p-6 font-mono">
     <div class="w-full flex mb-4 p-2 justify-end">
-        <x-jet-button>Create Tag</x-jet-button>
+        <x-jet-button wire:click="showCreateModal">Create Tag</x-jet-button>
     </div>
     <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
       <div class="w-full overflow-x-auto">
@@ -27,5 +27,34 @@
         </table>
       </div>
     </div>
+    <x-jet-dialog-modal wire:model="showTagModal">
+      <x-slot name="title">Create Tag</x-slot>
+      <x-slot name="content">
+        <div class="mt-10 sm:mt-0">            
+            <div class="mt-5 md:col-span-2 md:mt-0">
+              <form>
+                <div class="overflow-hidden shadow sm:rounded-md">
+                  <div class="bg-white px-4 py-5 sm:p-6">
+                    <div class="grid grid-cols-6 gap-6">
+                          
+                      <div class="col-span-6">
+                        <label for="street-address" class="block text-sm font-medium text-gray-700">Tag name</label>
+                        <input type="text" wire:model="tagName" autocomplete="street-address" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                      </div>
+        
+                    </div>
+                  </div>
+
+                </div>
+              </form>
+            </div>
+        
+        </div>
+      </x-slot>
+      <x-slot name="footer">
+        <x-jet-button wire:click="closeTagModal">Cancel</x-jet-button>
+        <button wire:click="createTag" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 mx-2">CREATE</button>
+      </x-slot>
+    </x-jet-dialog-modal>
   </section>
 
